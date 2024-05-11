dab753e5f4f625e257eba7a3ed2d34c3ff37cee3
simplynamu <kutenamrata193504@gmail.com>
2024-05-11 09:03:01 +0530

    first commit

---
+++ src/components/Employees.js
@@ -0,0 +1,15 @@
+const Employees = [{
+    id: "1",
+    Name: "Namu Kute",
+    Age: "20"
+}, {
+    id: "2",
+    Name: "lokesh Kute",
+    Age: "30"
+}, {
+    id: "3",
+    Name: "shital Kute",
+    Age: "35"
+}]
+
+export default Employees;
---
+++ src/components/Home.js
@@ -0,0 +1,58 @@
+import React, { Fragment } from "react";
+import { Button, Table } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Employees from "./Employees";
+import {Link,useNavigate} from 'react-router-dom'
+
+function Home() {
+let history = useNavigate();
+  const handleDelete=(id)=>{
+    var index=Employees.map(function(e){
+      return e.id
+    }).indexOf(id);
+    Employees.splice(index,1);
+history('/');
+
+  }
+  return (
+    <Fragment>
+      <div style={{ margin: "10rem" }}>
+        <table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Employees && Employees.length > 0 ? 
+             Employees.map((item) => {
+                  return (
+                    <tr>
+                      <td>{item.Name}</td>
+                      <td>{item.Age}</td>
+                      <td>
+                        <Link to={'/edit'}>
+                        <Button onClick={() => alert(item.id)}>EDIT
+                        </Button>
+                        </Link>
+                        &nbsp;
+                        <Button onClick={() => handleDelete(item.id)}>DELETE
+                        </Button>
+                      </td>
+                    </tr>
+                  );
+                })
+              : "No data available"}
+          </tbody>
+        </table>
+        <br></br>
+        <Link className='d-grid gap 2' to="/create">
+          <Button size="lg">Create</Button>
+        </Link>
+      </div>
+    </Fragment>
+  );
+}
+export default Home;
